323481db964a3f732f8fd6a7865639287d609e33
Kevin <m.kevin.zhou@gmail.com>
2024-07-28 16:03:53 +0200

    changed neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 require("neotex.core")
 require("neotex.bootstrap")
+vim.g.neovide_remember_window_size = false
+vim.g.neovide_scale_factor = 0.7
+vim.g.neovide_transparency = 0.75
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_cursor_vfx_particle_density = 12.0
